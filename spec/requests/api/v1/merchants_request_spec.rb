a61549acc159fc82654e10e3634e6712ef180af1
Carleigh Crockett <capleugh@gmail.com>
2020-01-27 19:12:01 -0700

    Add merchant api controller test

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'Merchants API' do
+  it 'sends a list of merchants' do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+  end
+end
