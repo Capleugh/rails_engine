956b1d14ba3a18d1a77152d9f2a1c0d919c03ed6
Carleigh Crockett <capleugh@gmail.com>
2020-01-28 15:36:58 -0700

    Create relationships between invoices and transactions

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,9 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
 
+  has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  enum status: [:shipped]
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Invoice, type: :model do
     it { should belong_to :customer }
     it { should belong_to :merchant }
 
+    it { should have_many :transactions}
     it { should have_many :invoice_items }
     it { should have_many(:items).through(:invoice_items)}
   end
