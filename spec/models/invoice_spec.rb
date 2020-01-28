3c9625ab0e84cf5eb3883cc9bd8e9b59fb424978
Carleigh Crockett <capleugh@gmail.com>
2020-01-28 00:18:50 -0700

    Establish relationships between models

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
+RSpec.describe Invoice, type: :model do
   describe "relationships" do
+    it { should belong_to :customer }
     it { should belong_to :merchant }
 
     it { should have_many :invoice_items }
-    it { should have_many(:invoices).through(:invoice_items) }
+    it { should have_many(:items).through(:invoice_items)}
   end
 end
