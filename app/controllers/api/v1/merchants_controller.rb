5366b8710758f35ca9d050defc71e15d8303672e
Carleigh Crockett <capleugh@gmail.com>
2020-01-28 23:49:47 -0700

    Add enpoints for merchant index and show

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,4 +1,11 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
+    merchants = Merchant.all
+    render json: MerchantSerializer.new(merchants)
+  end
+
+  def show
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
   end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -7,5 +7,20 @@ describe 'Merchants API' do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)["data"]
+
+    expect(merchants.count).to eq(3)
+  end
+
+  it 'can send a single merchant' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)["data"]["attributes"]
+    
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id)
   end
 end
