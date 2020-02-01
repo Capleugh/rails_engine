ef7c7d1af64b69df2933d72c26a7c9baa220961a
Carleigh Crockett <capleugh@gmail.com>
2020-02-01 16:30:04 -0700

    Hand roll find and find_all query parameters

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, param: :filter, only: [:index, :show]
+      scope :merchants, module: :merchants do
+        get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
+      end
+
+      resources :merchants, only: [:index, :show] do
+        scope module: :merchants do
+          resources :items, only: :index
+        end
+      end
     end
   end
 end
