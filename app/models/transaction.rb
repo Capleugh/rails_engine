ff8e3d8e993e71d4c745ffc4e4c89c2954624ad1
Carleigh Crockett <capleugh@gmail.com>
2020-02-03 06:29:52 -0700

    Create a scope for transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,10 @@ class Transaction < ApplicationRecord
   belongs_to :invoice
 
   enum result: [:fail, :success]
+
+  scope :successful, -> {where(result: 'success')}
+
+  def self.successful
+    where(result: 'success')
+  end
 end
