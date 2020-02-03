7185bc8730af56bbadab3ba7e52b36264b486d24
Carleigh Crockett <capleugh@gmail.com>
2020-02-03 06:28:34 -0700

    Add class model method to revenue controller for merchant with highest revenue

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def index
-    merchants = Merchant.limit(request.query_parameters)
+    limit_merchants = request.query_parameters["quantity"].to_i
+    merchants = Merchant.merchants_ranked_by_revenue(limit_merchants)
 
     render json: MerchantSerializer.new(merchants)
   end
