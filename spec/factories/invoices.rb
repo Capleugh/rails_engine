f4f356a238e319ee9374dac7745f42e4b480de40
Carleigh Crockett <capleugh@gmail.com>
2020-02-02 17:40:42 -0700

    Create order_invoices class method and test for ordering invoices by id

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -3,5 +3,7 @@ FactoryBot.define do
     customer { nil }
     merchant { nil }
     status { 0 }
+    created_at { Faker::Time.backward(days: 25, period: :morning, format: :short) }
+    updated_at { Faker::Time.backward(days: 10, period: :morning, format: :short) }
   end
 end
