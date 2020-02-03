2176f011956f6cd41d6665d41333dd6ae3849d9a
Carleigh Crockett <capleugh@gmail.com>
2020-02-02 17:41:42 -0700

    Create item_invoices class method and test for ordering items by id

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe Item, type: :model do
     it { should have_many :invoice_items }
     it { should have_many(:invoices).through(:invoice_items) }
   end
+
+  describe 'class methods' do
+    it "self.order_items can order items by item id" do
+      archer = create(:merchant)
+      item_1 = create(:item, merchant: archer)
+      item_2 = create(:item, merchant: archer)
+      item_3 = create(:item, merchant: archer)
+
+      expect(Item.order_items).to eq([item_1, item_2, item_3])
+    end
+  end
 end
