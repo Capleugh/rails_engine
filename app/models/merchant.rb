6c35bb2c48883eec3bf55893ade38e1e4a3c9dbb
Carleigh Crockett <capleugh@gmail.com>
2020-02-03 06:29:21 -0700

    Create merchants_ranked_by_revenue class method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,15 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
+
+  def self.merchants_ranked_by_revenue(limit)
+    joins(invoices: [:invoice_items, :transactions])
+    .select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue')
+    .group(:id)
+    .merge(Transaction.successful)
+    .order('revenue desc')
+    .limit(limit)
+  end
 end
